142973b5dd953f60d7f958e34a3ab6f7659b7ec2
ElWin95 <elvinma@code.edu.az>
2023-10-23 02:25:48 +0400

    '2'

---
+++ 01. Lesson/ShopAppP416/ShopAppP416/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopAppP416.Data;
+using ShopAppP416.Models;
+
+namespace ShopAppP416.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Products.Where(p=>!p.IsDelete).ToList());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetOne(int? id)
+        {
+            if (id == null) return BadRequest();
+            var product = _context.Products.FirstOrDefault(p=>p.Id == id);
+            if (product == null) return BadRequest();
+            return StatusCode(StatusCodes.Status200OK, product);
+        }
+    }
+}
---
+++ 01. Lesson/ShopAppP416/ShopAppP416/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopAppP416.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double SalePrice { get; set; }
+        public double CostPrice { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
