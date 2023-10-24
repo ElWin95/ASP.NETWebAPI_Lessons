3fbad102d90c277952a0706fd660007b7b0e2d8d
ElWin95 <elvinma@code.edu.az>
2023-10-25 01:05:02 +0400

    '3'

---
+++ 02. Lesson/ShopAppP416/ShopAppP416/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopAppP416.Models;
+
+namespace ShopAppP416.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
